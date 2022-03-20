7303351fec75da2510f74054d77b715f6ab8a5e7
zhangpengyf <zhangpengyf@gmail.com>
2022-03-20 20:30:59 +0800

    save

--- go/helloworld.go
+++ go/helloworld.go
@@ -1,6 +1,12 @@
 package main
+
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	//fmt.Println("Hello, World!")
+	var stockcode = 123
+	var enddate = "2020-12-31"
+	var url = "Code=%d&endDate=%s"
+	var target_url = fmt.Sprintf(url, stockcode, enddate)
+	fmt.Println(target_url)
 }
